7f06d7a49291bfb0fb45a1753e82459fd465beaa
Steve Steyaert <stephen.a.steyaert@gmail.com>
2026-05-25 23:01:51 -0400

    feat: add landing page, 500 error page, and root URL

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('whitelist/', include('whitelist.urls')),
     path('recs/', include('recs.urls')),
     path('reports/', include('reports.urls')),
